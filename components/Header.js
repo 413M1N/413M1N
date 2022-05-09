0459efbe419be6d4a964dcb23384ca25c6dfc2b2
413M1N <76206403+413M1N@users.noreply.github.com>
2022-05-09 11:34:46 +0300

    commit

--- components/Header.js
+++ components/Header.js
@@ -165,7 +165,7 @@ export default function Header() {
             </a>
 
             <a
-              href="./components/call.js"
+              href="./Contact"
               className="text-base font-semibold text-gray-700 hover:text-teal-700"
             >
               Contact Us
