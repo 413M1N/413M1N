b72647981229f3c5ac381037aed25a65624d65b2
413M1N <76206403+413M1N@users.noreply.github.com>
2022-06-16 10:59:26 +0300

    commit

--- pages/footwear/ladies.js
+++ pages/footwear/ladies.js
@@ -28,35 +28,38 @@ const products = [
   },
   {
     id: 2,
-    name: "Ladies Safety Shoe slip-in",
-    description: "Padded safety shoe,Padded insole,Microfiber leather,Steel mid-plate,Energy absorbing heel,Oil resistant,Steel toe,Has no laces",
-    imageSrc: "products/lacelad.jpg",
-    imageAlt: "Padded safety shoe,Padded insole,Microfiber leather,Steel mid-plate,Energy absorbing heel,Oil resistant,Steel toe,Has no laces.",
+    name: "Sporty safety shoe",
+    description: "",
+    imageSrc: "products/sporty.jpg",
+    imageAlt: ".",
   },
   {
     id: 3,
-    name: "Ladies/Men Safety Shoe",
-    description:"Breathable genuine leather,Steel toe,Steel plate,Oil & acid resistant,Energy absorbing heels,Padded insole with mesh lining",
-    imageSrc: "products/executiv.jpg",
-    imageAlt: "Person using a pen to cross a task off a productivity paper card.",
+    name: "Ladies safety shoe without laces",
+    description: "",
+    imageSrc: "products/lacelad.jpg",
+    imageAlt: ".",
   },
+
+  // More products...
 ];
 
-export default function ladies() {
+export default function Footwear() {
   return (
     <div className="bg-white-300">
       <div className="mx-auto max-w-7xl w-full pt-9 pb-2 text-center lg:text-centre">
         <h1 className="text-3xl tracking-tight mb-4 font-medium sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
           <p className="mt-10 text-centre text-3xl leading-8  tracking-tight text-teal-900 sm:text-4xl font-['Mochiy_Pop_P_One']">
-            LADIES FOOTWEAR
+           LADIES FOOTWEAR
           </p>
         </h1>
 
         <div className="mt-16 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
+            <a key={product.id}  className="group">
               <div className="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-lg  xl:aspect-w-15 xl:aspect-h-15">
-                <img src={products.imageSrc} alt={product.imageAlt} />
+                <image src={product.imageSrc}
+                 alt={product.imageAlt}/>
               </div>
               <h3 className="mt-4 text-sm text-white-100 font-['Mochiy_Pop_P_One']">
                 {product.name}
--- pages/uniforms/cleaning.js
+++ pages/uniforms/cleaning.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from "next/image";
 
 /* eslint-disable @next/next/no-img-element */
 /*
@@ -22,37 +23,41 @@ import React from 'react'
 const products = [
   {
     id: 1,
-    name: "Helmet with ventilation",
-    description:
-      "High-density polyethylene, unique crown ventilation system with plastic cradle,Sweat band-brushed cloth on foam,Touch and lightweight,Customized logo options",
-    imageSrc: "products/Helmetbreathe.jpg",
+    name: "Cleaning dress",
+    description: "",
+    imageSrc: "products/Cleanlad.png",
     imageAlt: "safety helmet with ventillation",
   },
-
+  {
+    id: 2,
+    name: "Cleaning dress",
+    description: "",
+    imageSrc: "products/Clean.jpg",
+    imageAlt: "safety helmet with ventillation",
+  },
 
   // More products...
 ];
 
-export default function Eyeppe () {
+export default function Cleaning () {
   return (
      <div className="bg-white-300">
      <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:text-centre">
           <h1 className="text-3xl tracking-tight mb-4 font-medium sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
 
-         <p className="mt-2 text-centre text-3xl leading-8  tracking-tight text-teal-900 sm:text-4xl font-['Mochiy_Pop_P_One']">
-           CLEANERS UNIFORM
+         <p className="mt-2 text-centre text-3xl leading-8  tracking-tight text-teal-900 sm:text-2xl font-['Mochiy_Pop_P_One']">
+           CLEANERS AND HOUSEKEEPING UNIFORM
           </p>
           </h1>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
+            <a key={product.id}  className="group">
               <div className="w-full aspect-w-5 aspect-h-5 bg-gray-200 rounded-lg  xl:aspect-w-15 xl:aspect-h-15">
                 <img
                   src={product.imageSrc}
                   alt={product.imageAlt}
-
-                />
+                 />
               </div>
               <h3 className="mt-4 text-sm text-white-100 font-['Mochiy_Pop_P_One']">{product.name}</h3>
               <p className="mt-1 text-sm  text-white-900">{product.description}</p>
